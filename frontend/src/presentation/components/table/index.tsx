d65c309d291e1ffff18fd68b26d65308eb561cf4
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-09-01 08:14:34 -0300

    feat: add table

--- frontend/src/presentation/components/table/index.tsx
+++ frontend/src/presentation/components/table/index.tsx
@@ -1,6 +1,3 @@
-import styles from './styles.module.scss'
-
-import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -8,55 +5,63 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { FC } from 'react';
+
+type Props = {
+    data: {
+        name: string,
+        id: number,
+        amount: number,
+        status: string,
+        due: string
+    }[]
+}
 
 const createData = (
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
+    name: string,
+    id: number,
+    amount: number,
+    status: string,
+    due: string
 ) => {
-  return { name, calories, fat, carbs, protein };
+    return { name, id, amount, status, due };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Felipe Saadi', 1, 32000, "Pago", "29/08/2024"),
+    createData('Paulo Brito', 1, 30000, "Pago", "17/05/2024")
 ];
 
-export const TableComponent = () => {
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+export const TableComponent: FC<Props> = ({ data }: Props) => {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Id</TableCell>
+                        <TableCell align="right">Name</TableCell>
+                        <TableCell align="right">amount</TableCell>
+                        <TableCell align="right">Status</TableCell>
+                        <TableCell align="right">Due</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow
+                            key={row.id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell >{row.id}</TableCell>
+                            <TableCell component="th" scope="row" align="right">
+                                {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.amount}</TableCell>
+                            <TableCell align="right">{row.amount}</TableCell>
+                            <TableCell align="right">{row.due}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
